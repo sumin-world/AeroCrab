7a22d1377c124604d3c004df12c0889ecf5f917b
sumin-world <tnalsdk0914@gmail.com>
2025-10-04 23:24:44 +0900

    chore: bootstrap AeroCrab (BitCrab Alumni Code Study)

---
+++ templates/template.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    // 빠른 입출력 (필요시)
+    // setvbuf(stdout, NULL, _IOFBF, 1<<20);
+    // setvbuf(stdin,  NULL, _IOFBF, 1<<20);
+
+    // TODO: 풀이 작성
+    // 예: int a, b; scanf("%d %d", &a, &b); printf("%d\n", a+b);
+    return 0;
+}
